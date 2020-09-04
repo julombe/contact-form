c429c207d0e9e06310d10c9beea0bf47bdf780bd
José Ulombe <jose.ulombe@hotmail.com>
2020-09-04 22:45:01 +0100

    Add file

--- server.js
+++ server.js
@@ -4,6 +4,20 @@
 const express = require('express')
 const server = express()
 
+//adicionando rotas//
+
+server.get("/", function(req, res) {
+    return res.send("Hi")
+})
+
+
+
+
+
+
+
+
+
 server.listen(3000, function() {
     console.log("server is running")
 })
